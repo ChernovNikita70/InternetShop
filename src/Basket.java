8063f16a22f80d413453002e754f8ae04f641280
nikita_chernov <nicitaa@inbox.ru>
2022-02-03 13:45:43 +0300

    homework 4.2

--- src/Basket.java
+++ src/Basket.java
@@ -4,20 +4,36 @@ public class Basket {
     private double totalWeight = 0;
     private int limit;
 
+    public Basket (){
+        items = "Список товаров: ";
+        this.limit = 10000;
+    }
+
     public Basket (int limit) {
+        this ();
         this.limit = limit;
     }
 
-    public void add (String name, int price, double Weight) {
+    public Basket (String items, int totalPrice) {
+        this();
+        this.items = this.items + items;
+        this.totalPrice = totalPrice;
+        this.totalWeight = getTotalWeight ();
+    }
+
+
+    public void add (String name, int price, int count, double weight){
         if (contains(name)){
             return;
         }
         if (totalPrice + price >= limit){
             return;
         }
-        items = items + "\n" + name + "-" + price + " рублей " + Weight + " грамм";
-        totalPrice = totalPrice + price;
-        totalWeight = totalWeight + Weight;
+        items = items + "\n" + name + " - " + count + " шт " + price + " рублей " + weight + " грамм";
+        totalPrice = totalPrice + count * price;
+        totalWeight = totalWeight + count * weight;
+
+
     }
     public boolean contains (String name) {
         return items.contains (name);
